02c56fd89e14624ca6ad39447185ba865b137241
Rui Oliveira <Rui Oliveira@ServDebt.local>
2018-02-09 17:59:28 +0000

    Removed echo and emojis

--- src/TelegramHandler.php
+++ src/TelegramHandler.php
@@ -13,7 +13,6 @@ use Monolog\Logger;
  * into telegram using it's API.
  *
  */
-
 class TelegramHandler extends AbstractProcessingHandler
 {
 
@@ -27,133 +26,105 @@ class TelegramHandler extends AbstractProcessingHandler
      *
      * @param int $level - The minimum logging level at which this handler will be triggered
      * @param string $bubble - Whether the messages that are handled can bubble up the stack or not
-	 *
+     *
      */
 
     public function __construct($level = Logger::DEBUG, $bubble = true)
     {
-		parent::__construct($level, $bubble);
+        parent::__construct($level, $bubble);
         if (!extension_loaded('curl')) {
             throw new Exception('curl is needed to use this library');
         }
     }
-	
-	/**
-	 * set bot token
-	 *
-	 * @param string $token
-	 * @return void
-	 */
-	public function setBotToken(string $token) {
-		$this->token = $token;
-	}
-	
-	/**
-	 * set message recipients
-	 *
-	 * @param array $recipients
-	 * @return void
-	 */
-	public function setRecipients(array $recipients) {
-		$this->recipients = $recipients;
-	}
-	
-	/**
-	 * set date format to be used on message
-	 * 
-	 * @param string $dateFormat
-	 * @param string $timezone_abbreviations_list
-	 * @return void
-	 */
-	public function setDateFormat(string $dateFormat, string $timeZone = 'UTC') {
-		$this->dateFormat = $dateFormat;
+
+    /**
+     * set bot token
+     *
+     * @param string $token
+     * @return void
+     */
+    public function setBotToken(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * set message recipients
+     *
+     * @param array $recipients
+     * @return void
+     */
+    public function setRecipients(array $recipients)
+    {
+        $this->recipients = $recipients;
+    }
+
+    /**
+     * set date format to be used on message
+     *
+     * @param string $dateFormat
+     * @param string $timezone_abbreviations_list
+     * @return void
+     */
+    public function setDateFormat(string $dateFormat, string $timeZone = 'UTC')
+    {
+        $this->dateFormat = $dateFormat;
         date_default_timezone_set($timeZone);
-	}
+    }
 
     /**
      * format the log to send
-     * @param $record[] log data
+     * @param $record [] log data
      * @return void
      */
     public function write(array $record)
     {
         $format = new LineFormatter;
         $context = $record['context'] ? $format->stringify($record['context']) : '';
-        $date =  date($this->dateFormat);
-        $message = $date . PHP_EOL . $this->getEmoji($record['level']) . ' ' . $record['message'] . $context;
+        $date = date($this->dateFormat);
+        $message = $date . PHP_EOL . $record['message'] . $context;
         $this->send($message);
 
     }
 
     /**
-     *    send log to telegram channel
-     *    @param string $message Text Message
-     *    @return void
+     * send log to telegram channel
+     *
+     * @param string $message Text Message
+     * @return boolean - success/failure
      *
      */
     public function send($message)
     {
-		
-      try{
-		  
-		if (!isset($this->token)) {
-			throw new Exception('No token added. No bot!');
-		}
-		if (!count($this->recipients)) {
-			throw new Exception('No recipients added. No one to send the message!');
-		}
-		
-        $ch = curl_init();
-        $url = self::host . $this->token . "/SendMessage";
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        foreach ($this->recipients as $recipient) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-				'text'    => $message,
-				'chat_id' => $recipient,
-			)));
-		
-			$result = curl_exec($ch);
-			$result = json_decode($result,1);
-			if($reult['ok'] === false){
-				echo  'telegram api response : ' .$result['description'];
-			}
-		}
-       }catch(Exception $error){
-         echo $error;
-       }
-    }
+        try {
+            if (!isset($this->token)) {
+                throw new Exception('No token added. No bot!');
+            }
+            if (!count($this->recipients)) {
+                throw new Exception('No recipients added. No one to send the message!');
+            }
 
-    /**
-     * make emoji for log events
-     * @return array
-     *
-     */
-    protected function emojiMap()
-    {
-        return [
-            Logger::DEBUG     => '🚧',
-            Logger::INFO      => '‍🗨',
-            Logger::NOTICE    => '🕵',
-            Logger::WARNING   => '⚡️',
-            Logger::ERROR     => '🚨',
-            Logger::CRITICAL  => '🤒',
-            Logger::ALERT     => '👀',
-            Logger::EMERGENCY => '🤕',
-        ];
-    }
+            $ch = curl_init();
+            $url = self::host . $this->token . "/SendMessage";
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            foreach ($this->recipients as $recipient) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+                    'text' => $message,
+                    'chat_id' => $recipient,
+                )));
 
-    /**
-     * return emoji for given level
-     *
-     * @param $level
-     * @return string
-     */
-    protected function getEmoji($level)
-    {
-        $levelEmojiMap = $this->emojiMap();
-        return $levelEmojiMap[$level];
+                $result = curl_exec($ch);
+                $result = json_decode($result, 1);
+                if ($result['ok'] === false) {
+                    return false;
+                }
+            }
+        } catch (Exception $error) {
+            return false;
+        }
+        return true;
     }
 
 }
